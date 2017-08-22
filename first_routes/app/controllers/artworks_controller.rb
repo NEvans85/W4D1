499e834a1498aee5443ddd4b40fb5401acaad876
Nicholas Evans <nevans85.pro@gmail.com>
2017-08-21 19:28:16 -0700

    write artwork controller, create artwork_shares controller

--- first_routes/app/controllers/artworks_controller.rb
+++ first_routes/app/controllers/artworks_controller.rb
@@ -1,2 +1,48 @@
 class ArtworksController < ApplicationController
+  def index
+    @artworks = Artwork.all
+    render json: @artworks
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 422
+    end
+  end
+
+  def delete
+    @artwork = Artwork.find(params[:id])
+    if @artwork.destroy
+      render json: @artwork
+    else
+      render plain: 'Artwork not present in database.'
+    end
+  end
+
+  def show
+    @artwork = Artwork.find(params[:id])
+    if @artwork
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 404
+    end
+  end
+
+  def update
+    @artwork = Artwork.find(params[:id])
+    if @artwork.update(artwork_params)
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 404
+    end
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:artist_id, :title, :img_url)
+  end
 end
